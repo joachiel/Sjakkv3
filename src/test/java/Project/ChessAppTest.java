45ba32cfddbad5cf055a6083bbe636c121d9adfd
Lars Mindrebø <larsmind@stud.ntnu.no>
2023-04-16 20:29:58 +0200

    FXML

---
+++ src/test/java/Project/ChessAppTest.java
@@ -0,0 +1,11 @@
+package Project;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import Project.Game.Board;
+
+public class ChessAppTest {
+    Board board;
+    
+}
